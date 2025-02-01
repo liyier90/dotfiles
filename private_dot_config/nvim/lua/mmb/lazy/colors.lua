c76aab6cd30f51e35a8748403d29e2622904d989
yier <56420072+liyier90@users.noreply.github.com>
2025-02-01 10:55:51 +0800

    change neovim color scheme

--- private_dot_config/nvim/lua/mmb/lazy/colors.lua
+++ private_dot_config/nvim/lua/mmb/lazy/colors.lua
@@ -1,17 +1,18 @@
 return {
     {
-        "liyier90/material.nvim",
-        name = "material",
-        branch = "original-colors",
+        "folke/tokyonight.nvim",
+        name = "tokyonight",
+        lazy = false,
+        priority = 1000,
         config = function()
-            require("material").setup({
-                async_loading = false,
+            require("tokyonight").setup({
+                style = "storm",
                 plugins = {
                     "nvim-cmp",
                     "telescope",
                 },
             })
-            vim.cmd("colorscheme material")
+            vim.cmd("colorscheme tokyonight")
 
             vim.cmd("highlight ColorColumn ctermbg=0 guibg=Black")
             vim.cmd("highlight LineNr ctermfg=8 guifg=DarkGrey")
