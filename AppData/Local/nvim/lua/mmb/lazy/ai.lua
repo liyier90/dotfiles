e94a0c2a98b46e2d8063a55ffcf071a2ffe2aad8
yier <56420072+liyier90@users.noreply.github.com>
2025-06-15 15:00:53 +0800

    add codecompanion

---
+++ AppData/Local/nvim/lua/mmb/lazy/ai.lua
@@ -0,0 +1,76 @@
+return {
+	{
+		"zbirenbaum/copilot.lua",
+		commit = "c1bb86abbed1a52a11ab3944ef00c8410520543d",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = true,
+		opts = {
+			copilot_model = "gpt-4.1",
+			panel = { enabled = false },
+			suggestion = { enabled = false },
+			filetypes = { ["*"] = false },
+		},
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		tag = "v16.3.0",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			{ "echasnovski/mini.pick", tag = "v0.16.0" },
+		},
+		keys = {
+			{ "<leader>ac", "<cmd>CodeCompanionChat Toggle<CR>", desc = "AI chat" },
+			{ "<leader>aa", "<cmd>CodeCompanionActions<CR>", desc = "AI actions" },
+		},
+		config = true,
+		opts = {
+			adapters = {
+				copilot = function()
+					return require("codecompanion.adapters").extend("copilot", {
+						schema = {
+							model = {
+								default = "gpt-4.1",
+							},
+						},
+					})
+				end,
+			},
+			display = {
+				action_palette = {
+					provider = "mini_pick",
+					opts = {
+						show_default_actions = true,
+						show_default_prompt_library = false,
+					},
+				},
+				chat = {
+					window = {
+						position = "right",
+					},
+				},
+				diff = {
+					enabled = true,
+					layout = "vertial",
+				},
+			},
+			strategies = {
+				chat = {
+					adapter = "copilot",
+				},
+				inline = {
+					adapter = "copilot",
+				},
+				cmd = {
+					adapter = "copilot",
+				},
+			},
+		},
+	},
+	{
+		"MeanderingProgrammer/render-markdown.nvim",
+		tag = "v8.5.0",
+		ft = { "markdown", "codecompanion" },
+	},
+}
--- AppData/Local/nvim/lua/mmb/lazy/init.lua
+++ AppData/Local/nvim/lua/mmb/lazy/init.lua
@@ -1,7 +1,7 @@
 return {
-    {
-        "nvim-lua/plenary.nvim",
-        tag = "v0.1.4",
-        lazy = true,
-    },
+	{
+		"nvim-lua/plenary.nvim",
+		commit = "857c5ac632080dba10aae49dba902ce3abf91b35",
+		lazy = true,
+	},
 }
