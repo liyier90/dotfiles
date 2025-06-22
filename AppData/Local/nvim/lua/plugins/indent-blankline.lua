edb4f0b7c665efd94b9fbb7c30c75b6ff33160f8
yier <56420072+liyier90@users.noreply.github.com>
2025-06-22 09:57:52 +0800

    add indent-blankline

---
+++ AppData/Local/nvim/lua/plugins/indent-blankline.lua
@@ -0,0 +1,15 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  tag = "v3.9.0",
+  main = "ibl",
+  opts = {
+    indent = {
+      char = "│",
+      tab_char = "│",
+    },
+    scope = {
+      enabled = false,
+    },
+  },
+  config = true,
+}
